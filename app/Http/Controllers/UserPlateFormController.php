c308b829a0a82ce60bc2fb624e46bcc94f499734
saifullah <solutionsofweb@gmail.com>
2022-04-19 01:19:56 +0500

    migration created

---
+++ app/Http/Controllers/UserPlateFormController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserPlateForm;
+use Illuminate\Http\Request;
+
+class UserPlateFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserPlateForm  $userPlateForm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserPlateForm $userPlateForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserPlateForm  $userPlateForm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserPlateForm $userPlateForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserPlateForm  $userPlateForm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserPlateForm $userPlateForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserPlateForm  $userPlateForm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserPlateForm $userPlateForm)
+    {
+        //
+    }
+}
